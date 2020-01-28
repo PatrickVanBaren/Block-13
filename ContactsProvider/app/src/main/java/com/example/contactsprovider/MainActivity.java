8ee2ee75aba1e6b90e8ceede1823e23e6b258a21
PatrickVanBaren <psvpsv63@gmail.com>
2020-01-28 22:52:07 +0300

    Create logic

--- ContactsProvider/app/src/main/java/com/example/contactsprovider/MainActivity.java
+++ ContactsProvider/app/src/main/java/com/example/contactsprovider/MainActivity.java
@@ -2,38 +2,48 @@ package com.example.contactsprovider;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.Manifest;
 import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
 
-    ContactsAdapter contactsAdapter;
+    RecyclerView mRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final RecyclerView contacts = new RecyclerView(this);
-        contactsAdapter = new ContactsAdapter(this);
-        contacts.setLayoutManager(new LinearLayoutManager(this));
-        contacts.setAdapter(contactsAdapter);
-
+        mRecyclerView = new RecyclerView(this);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        ActivityCompat.requestPermissions(this, new String[] { Manifest.permission.READ_CONTACTS}, 0);
+        Uri uri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
+        Cursor contacts = getContentResolver().query(uri, null, null, null, null);
+        mRecyclerView.setAdapter(new ContactsAdapter(this, contacts));
     }
 
     class ContactsAdapter extends RecyclerView.Adapter<ContactsAdapter.ViewHolder> {
 
         LayoutInflater mInflater;
+        Cursor mCursor;
 
-        public ContactsAdapter(final Context context) {
+        public ContactsAdapter(final Context context, Cursor cursor) {
             mInflater = LayoutInflater.from(context);
+            mCursor = cursor;
         }
 
         @NonNull
@@ -44,12 +54,22 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-
+            if (mCursor != null) {
+                mCursor.moveToPosition(position);
+                String string = String.format("%s, %s",
+                        mCursor.getString(mCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME)),
+                        mCursor.getString(mCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER)));
+                holder.textView.setText(string);
+            }
         }
 
         @Override
         public int getItemCount() {
-            return 0;
+            if(mCursor != null){
+                return mCursor.getCount();
+            } else {
+                return 0;
+            }
         }
 
         class ViewHolder extends RecyclerView.ViewHolder {
